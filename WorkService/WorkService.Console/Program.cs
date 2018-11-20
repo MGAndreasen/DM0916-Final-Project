17ddfd6a2c6947d37d5e4936e80b3073e52a5433
Michael Gerner Andreasen <geekcamp@gmail.com>
2018-11-20 14:01:12 +0100

    Minor changes

--- WorkService/WorkService.Console/Program.cs
+++ WorkService/WorkService.Console/Program.cs
@@ -6,7 +6,7 @@ namespace WorkService.Console
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            System.Console.WriteLine("Hello World!");
         }
     }
 }
