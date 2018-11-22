2ae979ecddc1172966ce936f809fe523d2bd2ecd
Michael Gerner Andreasen <geekcamp@gmail.com>
2018-11-22 12:48:25 +0100

    valid json

--- WebSites/playground/testjsondata/index.php
+++ WebSites/playground/testjsondata/index.php
@@ -18,14 +18,14 @@ foreach (new DirectoryIterator('cykler/') as $fileInfo) {
 	
 	if($fileInfo->isDir())
 	{
-		$out .="\"".$fileInfo->getFilename()."\" : {\n";
+		$out .="\"".$fileInfo->getFilename()."\" : [\n";
 		foreach (new DirectoryIterator('cykler/'.$fileInfo->getFilename().'/') as $category) {
 			if($category->isDot()) { continue; }
 			
-			$out .= "\"url\": \"http://4pi.dk/playground/testjsondata/index.php?cat=".$fileInfo->getFilename()."&img=".$category->getFilename() . "\",\n";
+			$out .= "{\"url\": \"http://4pi.dk/playground/testjsondata/index.php?cat=".$fileInfo->getFilename()."&img=".$category->getFilename() . "\"},\n";
 		}
 		$out = trim($out,",\n")."\n";
-		$out .= "},\n";
+		$out .= "],\n";
 	}
 }
 $out = trim($out,",\n");
