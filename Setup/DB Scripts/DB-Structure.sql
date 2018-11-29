dd6f91d6e6fd87a5cd4ece9b10e19dff429bdad7
Michael Gerner Andreasen <geekcamp@gmail.com>
2018-11-29 10:55:49 +0100

    model

--- Setup/DB Scripts/DB-Structure.sql
+++ Setup/DB Scripts/DB-Structure.sql
@@ -24,9 +24,12 @@ USE TFProject;
 -- -----------------------
 START TRANSACTION;
 SET FOREIGN_KEY_CHECKS=0;
-DROP TABLE IF EXISTS project_image;
+DROP TABLE IF EXISTS model_structur;
+DROP TABLE IF EXISTS model_structur_image;
+DROP TABLE IF EXISTS model;
+DROP TABLE IF EXISTS project_structur_image;
 DROP TABLE IF EXISTS customer;
-DROP TABLE IF EXISTS image;
+DROP TABLE IF EXISTS project_image;
 DROP TABLE IF EXISTS project;
 DROP TABLE IF EXISTS project_structur;
 SET FOREIGN_KEY_CHECKS=1;
@@ -56,7 +59,7 @@ CREATE TABLE IF NOT EXISTS `customer` (
 CREATE TABLE IF NOT EXISTS `project` (
   `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
   `customer_id` int(10) UNSIGNED NOT NULL,
-  `size` smallint(6) NOT NULL DEFAULT '256',
+  `image_size` smallint(6) NOT NULL DEFAULT '256',
   `enabled` tinyint(1) NOT NULL DEFAULT '0',
   `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   PRIMARY KEY (`id`),
@@ -68,25 +71,59 @@ CREATE TABLE IF NOT EXISTS `project_structur` (
   `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
   `project_id` int(10) UNSIGNED NOT NULL,
   `parent_id` int(10) UNSIGNED NOT NULL,
+  `image_size` smallint(5) UNSIGNED NOT NULL,
+  `filter_size` tinyint(4) NOT NULL,
+  `validation_size` tinyint(4) NOT NULL,
   `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   PRIMARY KEY (`id`),
   KEY `project_id` (`project_id`,`parent_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
--- Image
-CREATE TABLE IF NOT EXISTS `image` (
+-- Project_Image
+CREATE TABLE IF NOT EXISTS `project_image` (
   `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
   `project_structur_id` int(10) UNSIGNED NOT NULL,
   PRIMARY KEY (`id`),
   KEY `project_structur_id` (`project_structur_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
--- Project_Image
-CREATE TABLE IF NOT EXISTS `project_image` (
+-- Project_Structure_Image
+CREATE TABLE IF NOT EXISTS `project_structur_image` (
   `project_structur_id` int(10) UNSIGNED NOT NULL,
-  `image_id` int(10) UNSIGNED NOT NULL,
-  PRIMARY KEY (`project_structur_id`,`image_id`),
-  KEY `image_id` (`image_id`)
+  `project_image_id` int(10) UNSIGNED NOT NULL,
+  PRIMARY KEY (`project_structur_id`,`project_image_id`),
+  KEY `project_image_id` (`project_image_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- Model
+CREATE TABLE IF NOT EXISTS `model` (
+  `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `project_id` int(10) UNSIGNED NOT NULL,
+  `image_size` smallint(5) UNSIGNED NOT NULL,
+  `created` datetime NOT NULL,
+  `completed` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- Model_Structur
+CREATE TABLE IF NOT EXISTS `model_structur` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `model_id` int(10) UNSIGNED NOT NULL,
+  `parent_id` int(10) UNSIGNED NOT NULL,
+  `image_size` smallint(5) UNSIGNED NOT NULL,
+  `filter_size` tinyint(4) NOT NULL,
+  `validation_size` tinyint(4) NOT NULL,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  PRIMARY KEY (`model_id`,`id`),
+  KEY `parent_id` (`parent_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- Model_Structur_Image
+CREATE TABLE IF NOT EXISTS `model_structur_image` (
+  `model_structur_id` int(10) UNSIGNED NOT NULL,
+  `model_image_id` int(10) UNSIGNED NOT NULL,
+  PRIMARY KEY (`model_structur_id`,`model_image_id`),
+  KEY `model_image_id` (`model_image_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 COMMIT;
