1dd3c98ac530f1e7fd8bd0c38dfda4c0e401baab
Christoffer Sørensen <Christofferlund92@hotmail.com>
2018-11-29 12:09:16 +0100

    Updated customer.php and project.php, added projects to customer.php and added models to project.php

--- WebSites/classes/modelLayer/customer.php
+++ WebSites/classes/modelLayer/customer.php
@@ -3,6 +3,7 @@
 class Customer {
     private $id;
 	private $name;
+	private $projects = array();
 
 	public function __contruct(int $id, string $name){
 		$this->$id = $id;
@@ -17,12 +18,20 @@ class Customer {
 		return $this->$name;
 	}
 
+	public function getProjects(){
+		return $this->$projects;
+	}
+
 	public function setID(int $id){
-		$this->{$id} = $id;
+		$this->$id = $id;
 	}
 
 	public function setName(string $name){
-		$this->{$name} = $name;
+		$this->$name = $name;
+	}
+
+	public function setProjects($projects){
+		$this->$projects = $projects;
 	}
 }
 
--- WebSites/classes/modelLayer/project.php
+++ WebSites/classes/modelLayer/project.php
@@ -1,3 +1,58 @@
 <?php
 
+class Project {
+    private $id;
+	private $image_size;
+	private $created:
+	private $completed;
+	private $models = array();
+
+	public function __contruct(int $id, string $name, string $created, string $completed){
+		$this->$id = $id;
+		$this->$image_size = $image_size;
+		$this->$created = $created;
+		$this->$completed = $completed;
+	}
+
+	public function getID(){
+		return $this->$id;
+	}
+
+	public function getImage_size(){
+		return $this->$image_size;
+	}
+
+	public function getCreated(){
+		return $this->$created;
+	}
+
+	public function getCompleted(){
+		return $this->$completed;
+	}
+
+	public function getModels(){
+		return $models();
+	}
+
+	public function setID(int $id){
+		$this->{$id} = $id;
+	}
+
+	public function setÍmage_size(int $size){
+		$this->{$image_size} = $size;
+	}
+
+	public function setCreated($created){
+		$this->$created = $created;
+	}
+
+	public function setCompleted($completed){
+		$this->$completed = $completed;
+	}
+	
+	public function setModels($models){
+		$this->$models = $models; 
+	}
+}
+
 ?>
