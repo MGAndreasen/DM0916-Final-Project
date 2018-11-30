6f2aa12f524c792c84e4e35b85faf0321509ad09
Michael Gerner Andreasen <geekcamp@gmail.com>
2018-11-30 11:28:25 +0100

    cls

---
+++ WebSites/backend/cls.php
@@ -0,0 +1,27 @@
+<?php
+// Include our configuration
+$config = include('../config.php');
+
+// Init session
+session_start();
+
+// create output array
+$data = array();
+
+// Load Include files.
+include('../classes/dbLayer/connectionDB.php');
+
+// Connect to Mysql (MariaDB)
+$dbCtrl = new connectionDB();
+$conn = $dbCtrl->getConnection();
+
+
+// kode her under
+
+include('../classes/dbLayer/modelDB.php');
+
+$data['test'] = 'Hmm';
+
+// output data array as Json
+echo json_encode($data, JSON_PRETTY_PRINT);
+?>
