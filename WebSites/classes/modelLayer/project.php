c78de283e12949430bba833aa996eb008a00f215
Christoffer Sørensen <Christofferlund92@hotmail.com>
2018-12-06 15:26:22 +0100

    Fix project.php, syntax

--- WebSites/classes/modelLayer/project.php
+++ WebSites/classes/modelLayer/project.php
@@ -66,12 +66,12 @@ class Project implements JsonSerializable {
 	public function jsonSerialize() {
 		$jsonProjectStructes = [];
 		foreach($this->projectStructures as $projectStructure){
-			push_array($jsonProjectStructes, $projectStructure->JsonSerializable());
+			array_push($jsonProjectStructes, $projectStructure->JsonSerializable());
 		}
 
 		$jsonImages = [];
 		foreach($this->images as $image){
-			push_array($jsonImages, $image->JsonSerializable());
+			array_push($jsonImages, $image->JsonSerializable());
 		}
 
         return array (
