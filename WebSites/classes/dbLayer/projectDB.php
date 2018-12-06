24e49afe79ef61dfc3a912b9010947f790086653
Christoffer Sørensen <Christofferlund92@hotmail.com>
2018-12-06 15:59:15 +0100

    Fix projectDB.php

--- WebSites/classes/dbLayer/projectDB.php
+++ WebSites/classes/dbLayer/projectDB.php
@@ -48,7 +48,7 @@ class ProjectDB
 		if ($result->num_rows > 0) {
 			while($row = $result->fetch_assoc()) {
 				$projectStruture = new ProjectStruture($row['id'], $row['image_size'], $row['filter_size'], $row['validation_size'], $row['name']);
-				$subStructures = $this->getSubProjectStructures($row['parent_id']);
+				$subStructures = $this->getSubProjectStructures($row['id']);
 				
 				array_push($resultArr, $projectStruture);
 			}
