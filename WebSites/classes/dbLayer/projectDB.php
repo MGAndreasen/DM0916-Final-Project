71d7650cdbb46ba8cf4192fefd01040a8cf50828
Christoffer Sørensen <Christofferlund92@hotmail.com>
2018-12-06 15:21:21 +0100

    Fix projectDB.php, sntax

--- WebSites/classes/dbLayer/projectDB.php
+++ WebSites/classes/dbLayer/projectDB.php
@@ -25,7 +25,7 @@ class ProjectDB
 			while($row = $result->fetch_assoc()) {
 				$project = new Project($row['id'], $row['image_size'], $row['enabled'], $row['name']);
 				$projectStructures = $this->getProjectStructures((int)$row['id']); 
-				$project->setProjectStructures($projectStructes);
+				$project->setProjectStructures($projectStructures);
 				array_push($resultArr, $project);
 			}
 		} 
