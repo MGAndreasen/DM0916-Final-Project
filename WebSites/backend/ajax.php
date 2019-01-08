6416f255773738290ab95cdd74987c4e8d35c266
Michael Gerner Andreasen <geekcamp@gmail.com>
2019-01-08 16:04:55 +0100

    woot

--- WebSites/backend/ajax.php
+++ WebSites/backend/ajax.php
@@ -35,9 +35,9 @@ if(!empty($_GET['ctrl']) && mb_stripos($_GET['ctrl'], "..") === false && !empty(
 		// Load Include files.
 		require_once($path);
 
-		$theClass = $ctrl."Ctrl()";
-		$theCtrl = new $theClass;
-		//$theCtrl->$func();
+		$theClass = mb_ucfirst($ctrl)."Ctrl";
+		$theCtrl = new $theClass();
+		$theCtrl->$func();
 	}
 	else
 	{
