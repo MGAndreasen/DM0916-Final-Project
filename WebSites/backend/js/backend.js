d6f3a79ac2332b459af326ac4ed2f810bf69673f
Michael Gerner Andreasen <geekcamp@gmail.com>
2019-01-08 13:03:07 +0100

    fix

--- WebSites/backend/js/backend.js
+++ WebSites/backend/js/backend.js
@@ -1,5 +1,13 @@
 $(document).ready(function () {
 
+    $(document).ajaxError(function (event, xhr, settings) {
+            $("#status").text("The Ajax Error was: " + xhr.responseText);
+    });
+
+    $(document).ajaxSuccess(function (event, xhr, settings) {
+            $("status").text("Triggered ajaxSuccess handler. The Ajax response was: " + xhr.responseText);
+    });
+
     $.ajax({
         url: "/backend/ajax.php",
         data: {
