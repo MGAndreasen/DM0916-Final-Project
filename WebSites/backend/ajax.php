0370b76cd2c28d7dcd28a828414c2c40cfe65ad4
Michael Gerner Andreasen <geekcamp@gmail.com>
2019-01-08 13:49:08 +0100

    tesdt

--- WebSites/backend/ajax.php
+++ WebSites/backend/ajax.php
@@ -1,7 +1,7 @@
 <?php
-//header('Content-Type: text/html; charset=utf-8');
-//ini_set("default_charset", "UTF-8");
-//mb_internal_encoding("UTF-8");
+header('Content-Type: text/json; charset=utf-8');
+ini_set("default_charset", "UTF-8");
+mb_internal_encoding("UTF-8");
 //iconv_set_encoding("internal_encoding", "UTF-8");
 //iconv_set_encoding("output_encoding", "UTF-8");
 
@@ -15,29 +15,36 @@ session_start();
 $data = array();
 $error = array();
 
-$ctrl = basename(strtolower($_GET['ctrl']), ".php");
-$func = basename(strtolower($_GET['func']));
-$path = '../classes/ctrlLayer/'.$ctrl.'Ctrl.php';
-
-if (realpath($path))
+if((isset($_GET['ctrl']) && !empty($_GET['ctrl']) && mb_stripos($_GET['ctrl'], "..") === false) && (isset($_GET['func']) && !empty($_GET['func'])  && mb_stripos($_GET['func'], "..") === false))
 {
-	// Load Include files.
-	require_once('../classes/util/connectionDB.php');
+	$ctrl = basename(strtolower($_GET['ctrl']), ".php");
+	$func = basename(strtolower($_GET['func']));
+	$path = '../classes/ctrlLayer/'.$ctrl.'Ctrl.php';
 
-	// Connect to Mysql (MariaDB)
-	$dbCtrl = new connectionDB();
-	$conn = $dbCtrl->getConnection();
+	if (realpath($path))
+	{
+		// Load Include files.
+		require_once('../classes/util/connectionDB.php');
 
-	// Load Include files.
-	require_once($path);
+		// Connect to Mysql (MariaDB)
+		$dbCtrl = new connectionDB();
+		$conn = $dbCtrl->getConnection();
 
-	$theClass = mb_strtoupper($ctrl);
-	$theCtrl = new $theClass;
-	//$theCtrl->$func();
+		// Load Include files.
+		require_once($path);
+
+		$theClass = mb_strtoupper($ctrl);
+		$theCtrl = new $theClass;
+		//$theCtrl->$func();
+	}
+	else
+	{
+	array_push($error, errorMsg($ctrl, $func, 'Controller fil findes ikke.'));
+	}
 }
 else
 {
-	array_push($error, errorMsg($ctrl, $func, 'Controller fil findes ikke.'));
+	array_push($error, errorMsg($ctrl, $func, 'Ikke valid eller manglende Ctrl eller Func parameter!'));
 }
 
 // output data array as Json
