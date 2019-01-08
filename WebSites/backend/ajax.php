bcaf5bb5a16b4ac7742297e2f97e56d2a16fb0da
Michael Gerner Andreasen <geekcamp@gmail.com>
2019-01-08 14:09:42 +0100

    test

--- WebSites/backend/ajax.php
+++ WebSites/backend/ajax.php
@@ -14,9 +14,10 @@ session_start();
 // create empty output array
 $data = array();
 $error = array();
+@$ctrl = "" + $_GET['ctrl'];
+@$func = "" + $_GET['func'];
 
-
-if((isset($_GET['ctrl']) && !empty($_GET['ctrl']) && mb_stripos($_GET['ctrl'], "..") === false) && (isset($_GET['func']) && !empty($_GET['func'])  && mb_stripos($_GET['func'], "..") === false))
+if(!empty($_GET['ctrl']) && mb_stripos($_GET['ctrl'], "..") === false && !empty($_GET['func']))
 {
 	$ctrl = basename(strtolower($_GET['ctrl']), ".php");
 	$func = basename(strtolower($_GET['func']));
@@ -46,7 +47,7 @@ if((isset($_GET['ctrl']) && !empty($_GET['ctrl']) && mb_stripos($_GET['ctrl'], "
 }
 else
 {
-	@array_push($error, errorMsg($ctrl, $func, 'Ikke valid eller manglende Ctrl eller Func parameter!'));
+	array_push($error, errorMsg($ctrl, $func, 'Ikke valid eller manglende Ctrl eller Func parameter!'));
 }
 
 // output data array as Json
