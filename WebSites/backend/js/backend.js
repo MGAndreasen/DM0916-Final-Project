122574aff631bb00eb86e32a109366aeb593c0ba
Michael Gerner Andreasen <geekcamp@gmail.com>
2019-01-09 14:34:28 +0100

    sa

--- WebSites/backend/js/backend.js
+++ WebSites/backend/js/backend.js
@@ -16,7 +16,7 @@ $(document).ready(function () {
     //        func: "getProjects"
     //    }
     //});
-    var test = [1];
+    var test = [1,2];
     myPost('project', 'getProject', test);
 });
 
