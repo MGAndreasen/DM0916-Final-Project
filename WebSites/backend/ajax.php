62ed925d9d1cd2d2d64bca1de021a25fa8c9b060
Michael Gerner Andreasen <geekcamp@gmail.com>
2019-01-09 13:17:58 +0100

    dsa

--- WebSites/backend/ajax.php
+++ WebSites/backend/ajax.php
@@ -24,7 +24,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST')
 		@$func = $response['func'];
 		@$parms = array();
 
-		errorMsg('ewq','ewq','eqweqwe');
+		errorMsg('ewq','ewq',$response);
 	}
 }
 else
