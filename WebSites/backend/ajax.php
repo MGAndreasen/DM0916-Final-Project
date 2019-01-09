bd7bdb92409b80fd9f046124ff5d83f36397a155
Michael Gerner Andreasen <geekcamp@gmail.com>
2019-01-09 11:34:35 +0100

    test

--- WebSites/backend/ajax.php
+++ WebSites/backend/ajax.php
@@ -18,7 +18,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST')
 {
 	if (isset($_POST['data']))
 	{
-		$data['data'] = $_POST['data'];
+		$data['data'] = 1;
 	}
 	errorMsg("hmm","hmm2","looool post");
 }
