a28b6bd894668003a5d581b0e53a6be529c3e0e4
Michael Gerner Andreasen <geekcamp@gmail.com>
2019-01-09 13:56:42 +0100

    dsa

--- WebSites/backend/js/backend.js
+++ WebSites/backend/js/backend.js
@@ -22,7 +22,7 @@ $(document).ready(function () {
 
 function myPost(ctrl, func, parms) {
     var mydata = new Array({ "ctrl": ctrl, "func": func, "parms": parms });
-    var toSend = JSON.stringify(mydata[0]);
+    var toSend = JSON.stringify(mydata);
     console.log("SENDES:\n" + toSend);
     $.ajax({
         type: "POST",
