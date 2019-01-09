3c692ad2b3c38df7b833d4587da688b242fc63e8
Michael Gerner Andreasen <geekcamp@gmail.com>
2019-01-09 14:10:22 +0100

    dsa

--- WebSites/backend/ajax.php
+++ WebSites/backend/ajax.php
@@ -25,10 +25,10 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST')
 		@$func = $response['func'];
 		@$parms = $response['parms'];
 
-		if(!empty($_GET['ctrl']) && mb_stripos($_GET['ctrl'], "..") === false && !empty($_GET['func']))
+		if(!empty($ctrl) && mb_stripos($ctrl, "..") === false && !empty($func))
 		{
-			$ctrl = basename(strtolower($_GET['ctrl']), ".php");
-			$func = basename(strtolower($_GET['func']));
+			$ctrl = basename(strtolower($ctrl), ".php");
+			$func = basename(strtolower($func));
 
 			$path = '../classes/ctrlLayer/'.$ctrl.'Ctrl.php';
 
