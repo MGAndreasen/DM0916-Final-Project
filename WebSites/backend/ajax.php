170689f64b88a60048026b3ef3bcb9750de38f93
Michael Gerner Andreasen <geekcamp@gmail.com>
2019-01-09 12:15:13 +0100

    fds

--- WebSites/backend/ajax.php
+++ WebSites/backend/ajax.php
@@ -20,7 +20,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST')
 	if (isset($_REQUEST['resp']))
 	{
 		//var_dump($_POST['resp']);
-		$data['data'] = $_REQUEST['resp'];
+		$data['data'] = [1,2,3,4];
 	}
 	
 	//errorMsg("hmm","hmm2","looool post");
