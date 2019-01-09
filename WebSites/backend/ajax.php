ea01e8cf5d6948e5215b96c9e832a754ae13da00
Michael Gerner Andreasen <geekcamp@gmail.com>
2019-01-09 13:47:58 +0100

    dsa

--- WebSites/backend/ajax.php
+++ WebSites/backend/ajax.php
@@ -20,7 +20,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST')
 	{
 		@$response = json_decode(utf8_encode($_REQUEST['resp']), true);
 
-		@$ctrl = $response[0]['ctrl'];
+		@$ctrl = $response['ctrl'];
 		@$func = $response['func'];
 		@$parms = array();
 
