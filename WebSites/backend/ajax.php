aa288c6aeb8952cfb129d0b64ec29ed25f58dacf
Michael Gerner Andreasen <geekcamp@gmail.com>
2019-01-12 13:40:24 +0100

    ds

--- WebSites/backend/ajax.php
+++ WebSites/backend/ajax.php
@@ -2,8 +2,6 @@
 header('Content-Type: text/json; charset=utf-8');
 ini_set("default_charset", "UTF-8");
 mb_internal_encoding("UTF-8");
-//iconv_set_encoding("internal_encoding", "UTF-8");
-//iconv_set_encoding("output_encoding", "UTF-8");
 
 // Include our configuration
 $config = include('../config.php');
@@ -15,25 +13,20 @@ session_start();
 $data = array();
 $response = array();
 
-if ($_SERVER['REQUEST_METHOD'] === 'POST')
-{
-	if (isset($_REQUEST['resp']))
-	{
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+	if (isset($_REQUEST['resp'])) {
 		@$response = json_decode(utf8_encode($_REQUEST['resp']), true)[0];
-
 		@$ctrl = $response['ctrl'];
 		@$func = $response['func'];
 		@$parms = $response['parms'];
 
-		if(!empty($ctrl) && mb_stripos($ctrl, "..") === false && !empty($func))
-		{
+		if(!empty($ctrl) && mb_stripos($ctrl, "..") === false && !empty($func)) {
 			$ctrl = basename(strtolower($ctrl), ".php");
 			$func = basename(strtolower($func));
 
 			$path = '../classes/ctrlLayer/'.$ctrl.'Ctrl.php';
 
-			if (realpath($path))
-			{
+			if (realpath($path)) {
 				// Load Include files.
 				require_once('../classes/util/connectionDB.php');
 
@@ -43,83 +36,21 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST')
 
 				// Load Include files.
 				require_once($path);
-	
+				// Lav instans af klassen og afvikel funktionen med parameterne
 				$theClass = ucfirst($ctrl)."Ctrl";
-				$theCtrl = new $theClass();
-				//$theCtrl->$func();
+				$theCtrl = $theClass::getInstance();
 				call_user_func_array(array($theCtrl, $func), $parms);
-			}
-			else
-			{
-				errorMsg($ctrl, $func, 'Controller fil findes ikke.');
-			}
-		}
-		else
-		{
-			errorMsg($ctrl, $func, 'Ikke valid eller manglende Ctrl eller Func parameter!');
-		}
-	}
-	else
-	{
-		errorMsg(null, null, 'Json ikke korrekt posted.');
-	}
-}
-else
-{
-	@$ctrl = $_GET['ctrl'];
-	@$func = $_GET['func'];
-	if(!empty($_GET['ctrl']) && mb_stripos($_GET['ctrl'], "..") === false && !empty($_GET['func']))
-	{
-		$ctrl = basename(strtolower($_GET['ctrl']), ".php");
-		$func = basename(strtolower($_GET['func']));
-
-		$path = '../classes/ctrlLayer/'.$ctrl.'Ctrl.php';
-
-		if (realpath($path))
-		{
-			// Load Include files.
-			require_once('../classes/util/connectionDB.php');
-
-			// Connect to Mysql (MariaDB)
-			$dbCtrl = new connectionDB();
-			$conn = $dbCtrl->getConnection();
-
-			// Load Include files.
-			require_once($path);
-
-			$theClass = ucfirst($ctrl)."Ctrl";
-			$theCtrl = new $theClass();
-			$theCtrl->$func();
-		}
-		else
-		{
-			errorMsg($ctrl, $func, 'Controller fil findes ikke.');
-		}
-	}
-	else
-	{
-		errorMsg($ctrl, $func, 'Ikke valid eller manglende Ctrl eller Func parameter!');
-	}
-}
+			} else { errorMsg($ctrl, $func, 'Controller fil findes ikke.'); }
+		} else { errorMsg($ctrl, $func, 'Ikke valid eller manglende Ctrl eller Func parameter!'); }
+	} else { errorMsg(null, null, 'Json ikke korrekt posted.'); }
+} else { errorMsg(null, null, 'Request var ikke en HTTP POST.'); }
 
 // output data array as Json
-
 echo json_encode($data, JSON_PRETTY_PRINT);
 
-
-function errorMsg($ctrl, $func , $msg)
-{
+function errorMsg($ctrl, $func , $msg) {
 	global $data;
-
-	if(!isset($data['errors']))
-	{
-		$data['errors'] = array();
-	}
-
-	array_push($data['errors'], array(
-			'ERRCTRL' => $ctrl,
-			'ERRFUNC' => $func,
-			'ERRMSG' => $msg
-	));
+	if(!isset($data['errors'])) { $data['errors'] = array(); }
+	array_push($data['errors'], array( 'ERRCTRL' => $ctrl, 'ERRFUNC' => $func, 'ERRMSG' => $msg	));
 }
 ?>
--- WebSites/classes/ctrlLayer/projectCtrl.php
+++ WebSites/classes/ctrlLayer/projectCtrl.php
@@ -2,26 +2,36 @@
 // Includes
 require_once('../classes/dbLayer/projectDB.php');
 
-class ProjectCtrl
-{
+class ProjectCtrl {
+    private static $instance;
+	private final function __construct() {}
+    private final function __clone() {}
+
 	private $mDB = null;
 	private $data = null;
 
-	public function __construct()
-	{
+	public final static function getInstance() {
+        if(!self::$instance) {
+            self::$instance = new self;    
+        }
+        return self::$instance;
+    }
+
+	private function __construct() {
 		global $data;
 		$this->data = &$data;
 		$this->mDB = new ProjectDB();
+
+		// Validering af JWT samt rettigheder
+		// TBD
 	}
 
-	public function getProject($id)
-	{
+	public function getProject($id)	{
 		$someDataFromDB = $this->mDB->getProject($id);
 		array_push($this->data, $someDataFromDB);
 	}
 
-	public function getProjects($customerID)
-	{
+	public function getProjects($customerID) {
 		$someDataFromDB = $this->mDB->getProjects($customerID);
 		array_push($this->data, $someDataFromDB);
 	}
