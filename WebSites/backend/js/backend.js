4701f51532a6e8f875bff16be191549b8ff4d2a5
Michael Gerner Andreasen <geekcamp@gmail.com>
2019-01-16 13:49:12 +0100

    dsa

--- WebSites/backend/js/backend.js
+++ WebSites/backend/js/backend.js
@@ -116,6 +116,5 @@ function notify(title, msg) {
     var id = "#notify-"+notifyNum;
     var data = "<div class='notify' id='" + id + "'><div>" + title + "</div>" + msg + "</div>";
     $("#status").prepend(data);
-    alert(id);
-    $(id).delay(5000).fadeOut(250).delay(1000).remove();
+    $("#notify-1").delay(5000).fadeOut(250).delay(1000).remove();
 }
