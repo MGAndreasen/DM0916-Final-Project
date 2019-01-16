d06c99d2a42e1b334261ca3f389b817d46cc9f66
Michael Gerner Andreasen <geekcamp@gmail.com>
2019-01-16 20:33:37 +0100

    dsa

--- WebSites/backend/js/pages.js
+++ WebSites/backend/js/pages.js
@@ -60,7 +60,8 @@ function pageApiTest() {
         var func = $("#apitestFunc").val();
         var parms = JSON.parse($("#apitest form textarea").val());
         var result = myPost(ctrl, func, parms);
-        $("#apitestResult").html(""+JSON.stringify(result));
+        console.log("TEST: "+JSON.stringify(result));
+        //$("#apitestResult").html(""+JSON.stringify(result));
     });
 
     
