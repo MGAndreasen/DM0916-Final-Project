31358eb2a6c8246d82e29a8f718e581d0d95bae5
Michael Gerner Andreasen <geekcamp@gmail.com>
2019-01-17 21:52:14 +0100

    ds

--- WebSites/backend/js/pageProjects.js
+++ WebSites/backend/js/pageProjects.js
@@ -1,24 +1,29 @@
 function page_projects() {
     var section = $('#projects');
-    var project_list = $('#project_list');
-
     section.html(projects_Layout());
+
+    populate_projectList();
+
+    
+}
+
+function projects_Layout() {
+    return "<h1>projects</h1>"
+        + "<div id='project_list'></div>";
+}
+
+function populate_projectList() {
+    var project_list = $('#project_list');
 
     // Test load data.
     var customerid = [1];
     var restData = myPost('project', 'getProjects', customerid);
     var p;
 
-    console.log(restData['status']);
-    //if (restData['status'] === "OK") {
+    if (restData['status'] === "OK") {
         $.each(restData['result']['projects'], function (key, value) {
             p = "<div id='project-" + value['id'] + "'>" + value['name'] + "</div>";
             project_list.append(p);
         });
-    //}
-}
-
-function projects_Layout() {
-    return "<h1>projects</h1>"
-        + "<div id='project_list'></div>";
+    }
 }
