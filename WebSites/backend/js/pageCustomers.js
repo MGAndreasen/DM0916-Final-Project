96cb3be7dc09ace3e01fff51bcaa918a2f94805e
Christoffer Sørensen <Christofferlund92@hotmail.com>
2019-01-18 15:12:07 +0100

    Fix pageCustomers.js, typo

--- WebSites/backend/js/pageCustomers.js
+++ WebSites/backend/js/pageCustomers.js
@@ -20,7 +20,7 @@ function page_customers() {
 
     // Test load data.
     var customerid = [1];
-    var restData = myPost('customers', 'getCustomer', customerid);
+    var restData = myPost('customer', 'getCustomer', customerid);
 
     $.each(restData['result']['customers'], function (key, value) {
         var p = "<div id='customer-" + value['id'] + "'>" + value['email'] + "</div>";
