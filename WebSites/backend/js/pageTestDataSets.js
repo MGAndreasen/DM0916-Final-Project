1c4902bb7b7c2972a2f211fc4ea7aad080eed752
Michael Gerner Andreasen <geekcamp@gmail.com>
2019-01-19 00:47:05 +0100

    dsa

--- WebSites/backend/js/pageTestDataSets.js
+++ WebSites/backend/js/pageTestDataSets.js
@@ -11,10 +11,10 @@ function page_testdatasets() {
     var filedock = $('filedock');
     filedock.off(); // remove eventhandlers
 
-    filedock.on('dragover', testdatasets_dragover(e));
-    filedock.on('dragenter', testdatasets_dragenter(e));
-    filedock.on('dragleave', testdatasets_dragleave(e));
-    filedock.on('drop', testdatasets_dragdrop(e));
+    filedock.on('dragover', testdatasets_dragover());
+    filedock.on('dragenter', testdatasets_dragenter());
+    filedock.on('dragleave', testdatasets_dragleave());
+    filedock.on('drop', testdatasets_dragdrop());
 }
 
 function testdatasets_createLayout() {
@@ -25,7 +25,7 @@ function testdatasets_createLayout() {
         + "<div id='file-result'></div >";
 }
 
-function testdatasets_dragover(e) {
+function testdatasets_dragover() {
     return function (e) {
         $(this).attr('class', 'filedock_hover');
         e.preventDefault();
@@ -34,7 +34,7 @@ function testdatasets_dragover(e) {
 }
 
 
-function testdatasets_dragenter(e) {
+function testdatasets_dragenter() {
     return function (e) {
         e.preventDefault();
         e.stopPropagation();
@@ -42,14 +42,14 @@ function testdatasets_dragenter(e) {
 }
 
 
-function testdatasets_dragleave(e) {
+function testdatasets_dragleave() {
     return function (e) {
         $(this).attr('class', 'filedock');
     };
 }
 
 
-function testdatasets_dragdrop(e) {
+function testdatasets_dragdrop() {
     return function (e) {
         if (e.originalEvent.dataTransfer) {
 
