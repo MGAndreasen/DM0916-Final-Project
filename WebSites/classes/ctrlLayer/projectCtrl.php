5cad6786f55b245327606808800ff6cec7271882
Christoffer Sørensen <Christofferlund92@hotmail.com>
2019-01-19 19:17:20 +0100

    Fix projectCtrl, typo

--- WebSites/classes/ctrlLayer/projectCtrl.php
+++ WebSites/classes/ctrlLayer/projectCtrl.php
@@ -47,7 +47,7 @@ class ProjectCtrl {
 
 		//Check if inserted
 		if ($newProjectId > 0) {
-			getCustomer($newProjectId);
+			getProject($newProjectId);
 		}
 		else {
 			errorMsg('ProjectCtrl','createProject()','Did not insert project correctly');
