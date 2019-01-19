70dd9ce108f31ffefc9656b1a09a8e1811681dc9
Michael Gerner Andreasen <geekcamp@gmail.com>
2019-01-19 14:28:09 +0100

    dsa

--- WebSites/classes/dbLayer/customerDB.php
+++ WebSites/classes/dbLayer/customerDB.php
@@ -42,7 +42,7 @@ class CustomerDB
 		
 		if ($result->num_rows > 0) {
 			while($row = $result->fetch_assoc()) {
-					$customer = new Customer($row['id'], $row['enabled'], $row['hash'], $row['salt'], 'ds', 'fd', $row['email']);
+					$customer = new Customer($row['id'], $row['enabled'], $row['hash'], $row['salt'], $row['created'], $row['last_access'], $row['email']);
 					array_push($resultArr, $customer);
 				}
 		}
