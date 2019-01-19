d8ef04539c1277d4848fb3e36009f50c964dbbdc
Michael Gerner Andreasen <geekcamp@gmail.com>
2019-01-20 00:11:53 +0100

    fds

--- WebSites/classes/dbLayer/projectDB.php
+++ WebSites/classes/dbLayer/projectDB.php
@@ -124,6 +124,9 @@ class ProjectDB
 		$query->execute();
 		//$result = $query->get_result();
 
+		//$conn->affected_rows
+		//$query->affected_rows
+
 		if ($query->num_rows > 0) {
 			errorMsg('fdfdsfs');
 			 $resultArr = $this->getProject($id);
