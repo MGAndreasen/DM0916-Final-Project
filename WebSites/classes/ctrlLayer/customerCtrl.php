b37e26ed1893235e5c2f20ef61046e6932c9a13d
Michael Gerner Andreasen <geekcamp@gmail.com>
2019-01-19 16:29:48 +0100

    df

--- WebSites/classes/ctrlLayer/customerCtrl.php
+++ WebSites/classes/ctrlLayer/customerCtrl.php
@@ -60,9 +60,10 @@ class CustomerCtrl {
 				// 3) sammenlign resultatet med hash_fra_db, er de ens har man tastet korrekt password, ellers ikke!
 
 				$newCustomerId = $this->mDB->createCustomer($email, $hash, $salt);
-
+				errorMsg('hmm','1','1');
 				if (!empty($result))
 				{
+					errorMsg('hmm','1','2');
 					// Lets try and fetch
 					getCustomer($newCustomerId);
 				}
