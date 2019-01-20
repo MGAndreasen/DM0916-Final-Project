cf97c4b79af95c78da966c6f95ac335944b92489
Michael Gerner Andreasen <geekcamp@gmail.com>
2019-01-20 02:44:42 +0100

    dsa

--- WebSites/backend/js/backend.js
+++ WebSites/backend/js/backend.js
@@ -43,8 +43,8 @@ $(document).ready(function () {
 
 //-- SITE Funcs
 function handlePagechange() {
-    if ($('#pages .active').length) {
-        var pageName = $('#pages .active').attr('id');
+    if ($('#pages > .active').length) {
+        var pageName = $('#pages > .active').attr('id');
         if ($('#' + pageName).length) {
             window['page_' + pageName]();
         }
