bf149432c762ea1f2f0fc1c06d15b6379084c2e2
Michael Gerner Andreasen <geekcamp@gmail.com>
2019-01-20 12:26:56 +0100

    dsa

--- WebSites/backend/js/pageProjects.js
+++ WebSites/backend/js/pageProjects.js
@@ -8,7 +8,7 @@ function page_projects() {
 
     $("#projects .project_hirachy .content > ul .sortable").sortable({
         connectWith: ".connected"
-    }).disableSelection();
+    });
 
     $("#sortable1, #sortable2").sortable({
         connectWith: ".connectedSortable"
@@ -144,7 +144,7 @@ function projects_create_hirachy_element() {
 
         if (name.length) {
             //var element = "<!-- Element start--><li id='project_hirachy_element_" + name + "' class='ui-state-default'><div>" + name + "</div><ul class='sortable'></ul></li><!-- Element end-->";
-            var element = "<!-- Element start--><li id='project_hirachy_element_" + name + "' class='ui-state-default'>" + name +"<ul class='sortable connected'></ul></li><!-- Element end-->";
+            var element = "<li id='project_hirachy_element_" + name + "' class='ui-state-default'>" + name +"</li>";
         var project_hirachy = $('#projects .project_hirachy > .content > ul');
         project_hirachy.append(element);
 
