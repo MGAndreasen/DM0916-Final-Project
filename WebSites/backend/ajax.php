647dd4c60bed436a582d4fea555f568c20c096d3
Michael Gerner Andreasen <geekcamp@gmail.com>
2019-01-21 01:03:50 +0100

    dsa

--- WebSites/backend/ajax.php
+++ WebSites/backend/ajax.php
@@ -1,55 +1,35 @@
 <?php
-header('Content-Type: text/json; charset=utf-8');
-ini_set("default_charset", "UTF-8");
-mb_internal_encoding("UTF-8");
-
-// Include our configuration
-$config = include('../config.php');
-
-// Init session
-session_start();
-
-// create empty output array
-$data = array();
-$response = array();
-
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-	if (isset($_REQUEST['resp'])) {
+header('Content-Type: text/json; charset=utf-8');	// Set HTTP header
+ini_set("default_charset", "UTF-8");				// Sets default_charset
+mb_internal_encoding("UTF-8");						// Set php multibyte encoding charaterset UTF8
+$config = include('../config.php');					// Returns config array from our configfile
+require_once('../classes/util/utils.php');			// Includes utility functions
+require_once('../classes/util/connectionDB.php');	// Includes dbHandler 
+session_start();									// Init session
+$data = array();									// create empty output array
+$response = array();								// Holds response
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {		// Check HTTP REQUEST_METHOD
+	if (isset($_REQUEST['resp'])) {					// And if we got a resp field
 		@$response = json_decode(utf8_encode($_REQUEST['resp']), true)[0];
 		@$ctrl = $response['ctrl'];
 		@$func = $response['func'];
 		@$parms = $response['parms'];
-
 		if(!empty($ctrl) && mb_stripos($ctrl, "..") === false && !empty($func)) {
 			$ctrl = basename(strtolower($ctrl), ".php");
 			$func = basename(strtolower($func));
-
 			$path = '../classes/ctrlLayer/'.$ctrl.'Ctrl.php';
-
 			if (realpath($path)) {
-				// Load Include files.
-				require_once('../classes/util/connectionDB.php');
-
-				// Connect to Mysql (MariaDB)
-				$dbCtrl = new connectionDB();
+				$dbCtrl = new connectionDB();		// Connect to Mysql (MariaDB)
 				$conn = $dbCtrl->getConnection();
-
-				// Load Include files.
-				require_once($path);
+				require_once($path); // Load Ctrl files.
 				// Lav instans af klassen og afvikel funktionen med parameterne
 				$theClass = ucfirst($ctrl)."Ctrl";
 				if (class_exists($theClass)) {
 					$theCtrl = $theClass::getInstance();
-
 					foreach (get_class_methods($theCtrl) as $method) {
-					    if (strtolower($method) != strtolower($func)) {
-							continue;
-					    }
-						else {
-							$func = $method;
-						}
+					    if (strtolower($method) != strtolower($func)) { continue; }
+						else { $func = $method; }
 					}
-
 					if(method_exists($theClass, $func)) {
 						@call_user_func_array(array($theCtrl, $func), $parms);
 					} else { errorMsg('Ajax.php', $func, 'Did not find methode: '.$func.' on class: '. $theClass); }
@@ -58,18 +38,11 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 		} else { errorMsg($ctrl, $func, 'Ikke valid eller manglende Ctrl eller Func parameter!'); }
 	} else { errorMsg(null, null, 'Json ikke korrekt posted.'); }
 } else { errorMsg(null, null, 'Request var ikke en HTTP POST.'); }
-
-// Hvis ingen fejl, send OK status med tilbage
-if(empty($data['errors'])) {
-	$data['status'] = "OK";
-}
-
-// output data array as Json
-echo json_encode($data, JSON_PRETTY_PRINT);
-
-function errorMsg($ctrl, $func , $msg) {
-	global $data;
-	if(!isset($data['errors'])) { $data['errors'] = array(); }
-	array_push($data['errors'], array( 'ERRCTRL' => $ctrl, 'ERRFUNC' => $func, 'ERRMSG' => $msg	));
-}
+if(empty($data['errors'])) { $data['status'] = "OK"; } // Hvis ingen fejl, send OK status med tilbage
+echo json_encode($data, JSON_PRETTY_PRINT); // output data array as Json
 ?>
+
+
+
+
+
